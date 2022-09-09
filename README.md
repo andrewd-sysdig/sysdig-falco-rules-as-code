de510c0fb335f40887c08d9c3dcfbe6eed7d1780
Andrew D <andrew.d@sysdig.com>
2022-09-09 17:21:30 +1000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# sysdig-falco-gitops
+# Managing your Sysdig Custom Falco Rules as code
 Example workflow on how to manage your custom falco rules &amp; exceptions as code
+
+- Manage your custom falco rules including exceptions in the `falco_rules_local.yaml` file. 
+- Use the `update_falco_rules_local.sh` file to push the new file to Sysdig. 
+- Warning this will overwrite the Custom Rules file so any changes anyone has made to that file in the UI will be replaced with the contents of this file
+- See the [Falco documentation](https://falco.org/docs/rules/) for the syntax of a Falco rule
+- Checkout the [free Falco 101 Training](https://falco.org/training/)
+
+## Usage
+Two environment variables are required:
+- `SECURE_API_TOKEN` - This is your Sysdig Secure API Token (note this is different to your ACCESS KEY)
+- `API_ENDPOINT` - This is the base URL of your Sysdig region
+
+`SECURE_API_TOKEN=xxx API_ENDPOINT=app.au1.sysdig.com ./update_falco_rules_local.sh`
---
+++ update_falco_rules_local.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$SECURE_API_TOKEN" ]; then
+	echo "Error: SECURE_API_TOKEN environment variable is not set"
+    exit 1
+fi
+
+if [ -z "$API_ENDPOINT" ]; then
+	echo "Error: API_ENDPOINT environment variable is not set. Example export API_ENDPOINT=app.au1.sysdig.com"
+    exit 1
+fi
+
+if [ ! -f falco_rules_local.yaml ]; then
+	echo "falco_rules_local.yaml does not exist in current directory"
+    exit 1
+fi
+
+FALCO_RULES_FILE="https://${API_ENDPOINT}/api/settings/falco/customRulesFiles/falco_rules_local.yaml"
+
+# Get version of current rules file as this is required to update them
+version=$(curl --silent --output /dev/null --show-error --request GET --url ${FALCO_RULES_FILE} --header 'content-type: application/json' --header "Authorization: Bearer ${SECURE_API_TOKEN}" | jq .version)
+
+# Get content of the falco_rules_local.yaml and convert to a single line with escaped newline \n characters
+# Note: -z doesn't work on MAC - try zcat if you are on MAC
+CONTENT=$(cat falco_rules_local.yaml | sed -z 's/\n/\\n/g;s/\\n$/\n/')
+
+# Call the Sysdig customRulesFiles API to push our new rules file 
+curl -w "%{http_code}\n" --silent --output /dev/null --show-error --request PUT --url ${FALCO_RULES_FILE} --header 'content-type: application/json' --header "Authorization: Bearer ${SECURE_API_TOKEN}" \
+  -d '{"filename":"falco_rules_local.yaml","content":"'"${CONTENT}"'","version":'"${version}"'}'

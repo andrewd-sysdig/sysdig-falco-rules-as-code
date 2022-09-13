524ad0892598b97b82822283c7859c022debd7d9
Andrew D <andrew.d@sysdig.com>
2022-09-13 22:17:06 +1000

    Add status code return check

--- update_falco_rules_local.sh
+++ update_falco_rules_local.sh
@@ -18,16 +18,19 @@ fi
 FALCO_RULES_FILE="https://${API_ENDPOINT}/api/settings/falco/customRulesFiles/falco_rules_local.yaml"
 
 # Get version of current rules file as this is required to update them
-version=$(curl --output /dev/null --show-error --request GET --url ${FALCO_RULES_FILE} --header 'content-type: application/json' --header "Authorization: Bearer ${SECURE_API_TOKEN}" | jq .version)
+version=$(curl --show-error --request GET --url ${FALCO_RULES_FILE} --header 'content-type: application/json' --header "Authorization: Bearer ${SECURE_API_TOKEN}" | jq .version)
 
 # Get content of the falco_rules_local.yaml and convert to a single line with escaped newline \n characters
 # Note: -z doesn't work on MAC - try zsed if you are on MAC
 CONTENT=$(cat falco_rules_local.yaml | sed -z 's/\n/\\n/g;s/\\n$/\n/')
 
-echo $CONTENT
-
 # Call the Sysdig customRulesFiles API to push our new rules file 
-curl -w "%{http_code}\n" --show-error --request PUT --url ${FALCO_RULES_FILE} --header 'content-type: application/json' --header "Authorization: Bearer ${SECURE_API_TOKEN}" \
-  -d '{"filename":"falco_rules_local.yaml","content":"'"${CONTENT}"'","version":'"${version}"'}'
+response=$(curl -w "%{http_code}\n" --silent --output /dev/null --show-error --request PUT --url ${FALCO_RULES_FILE} --header 'content-type: application/json' --header "Authorization: Bearer ${SECURE_API_TOKEN}" \
+  -d '{"filename":"falco_rules_local.yaml","content":"'"${CONTENT}"'","version":'"${version}"'}')
 
-  # --silent --output /dev/null
+# Evaluate reponse from curl and if anything but 200 return failure
+if [ "$response" -eq 200 ]; then
+  exit 0
+else
+  exit 1
+fi
